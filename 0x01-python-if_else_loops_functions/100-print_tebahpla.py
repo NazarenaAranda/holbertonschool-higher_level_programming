6dcc5fa8203b4a4be9657867505cefa2a88c13cb
NazarenaAranda <nazarenaaranda15@gmail.com>
2022-04-26 19:48:56 -0700

    A

--- 0x01-python-if_else_loops_functions/100-print_tebahpla.py
+++ 0x01-python-if_else_loops_functions/100-print_tebahpla.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
-for letras in range(122, 97, -1):
+for letras in range(122, 96, -1):
     if letras % 2:
         letras = letras - 32
     print("{:c}" . format(letras), end='')

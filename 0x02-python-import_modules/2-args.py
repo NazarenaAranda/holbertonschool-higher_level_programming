b896097d5bf04a08f09f5d7fdea7182adf67d878
NazarenaAranda <nazarenaaranda15@gmail.com>
2022-05-01 13:33:52 -0700

    argv

---
+++ 0x02-python-import_modules/2-args.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+if __name__ == "__main__":
+    import sys
+    lsn = len(sys.argv) - 1
+    print(f"{lsn:d} ",end='')
+    if lsn == 0:
+        print("arguments:.")
+    if lsn == 1:
+        print("argument:")
+    if lsn > 1:
+        print("argumments:")
+    aux = 0
+    for a in sys.argv:
+        if a != "./2-args.py":
+            aux += 1
+            print(f"{aux}: {a}")

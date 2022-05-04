c4348b79450e4c11fc6f88ccd221c18bc77d4bd1
NazarenaAranda <nazarenaaranda15@gmail.com>
2022-05-04 16:51:46 -0700

    arreglo del return

--- 0x04-python-more_data_structures/0-square_matrix_simple.py
+++ 0x04-python-more_data_structures/0-square_matrix_simple.py
@@ -3,4 +3,4 @@ def square_matrix_simple(matrix=[]):
     nueva = matrix.copy()
     for n in range(len(matrix)):
         nueva[n] = (list(map(lambda x: x * x, nueva[n])))
-        return nueva
+    return nueva

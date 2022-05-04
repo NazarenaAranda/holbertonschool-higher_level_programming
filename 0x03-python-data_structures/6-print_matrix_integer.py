88be3f5cd6e1eee93386318de7533ac12a717592
NazarenaAranda <nazarenaaranda15@gmail.com>
2022-05-03 19:51:25 -0700

    a,b

---
+++ 0x03-python-data_structures/6-print_matrix_integer.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+def print_matrix_integer(matrix=[[]]):
+    for a in matrix:
+        for b in a:
+            print(f"{b}", end='')
+            if b != a[-1]:
+                print(" ", end='')
+        print()
+            

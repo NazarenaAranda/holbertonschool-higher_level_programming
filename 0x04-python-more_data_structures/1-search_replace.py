4d0c355e142abebc6551db015ac6e3a5c1334f3a
NazarenaAranda <nazarenaaranda15@gmail.com>
2022-05-04 17:55:34 -0700

    pycode

--- 0x04-python-more_data_structures/1-search_replace.py
+++ 0x04-python-more_data_structures/1-search_replace.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 def search_replace(my_list, search, replace):
     nueva = my_list[:]
-    for n in range (len(nueva)):
+    for n in range(len(nueva)):
         if nueva[n] == search:
             nueva[n] = replace
     return nueva

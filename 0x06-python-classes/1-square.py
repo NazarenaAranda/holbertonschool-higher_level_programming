28047f42fe7d025f8fb7c5a90866493099f46bbf
NazarenaAranda <nazarenaaranda15@gmail.com>
2022-05-17 18:30:13 -0700

    1

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+
+""" creando una lase """
+
+
+class Square:
+    """funcion para inicializar la clase"""
+    def __init__(self, size):
+        """ dfghjk """
+        self.__size = size

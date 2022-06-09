dd21b5d35f0c5da5f4c0a34b86362762dae6af14
NazarenaAranda <nazarenaaranda15@gmail.com>
2022-06-09 14:27:57 -0700

    clase student

---
+++ 0x0B-python-input_output/9-student.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+
+"""crear una clase"""
+
+
+class Student:
+    """ iniciar """
+    def __init__(self, first_name, last_name, age):
+        """atributos de instancia publicos"""
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
+ 
+    def to_json(self):
+        """ devolver dict"""
+
+        return self.__dict__

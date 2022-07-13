dc54497c011ea49a81df630921e1a7eefb981fef
NazarenaAranda <nazarenaaranda15@gmail.com>
2022-07-13 13:13:26 -0700

    instance o no

---
+++ 0x0A-python-inheritance/3-is_kind_of_class.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python3
+""" funcion qe devuelva true o false """
+
+
+def is_kind_of_class(obj, a_class):
+    """si el objeto es una instancia devuelve True"""
+    return isinstance(obj, a_class)

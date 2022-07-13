6a5f9406bcd80a13cc14dd0a8441284ffef419ee
NazarenaAranda <nazarenaaranda15@gmail.com>
2022-07-13 12:27:12 -0700

    lista ordenada

---
+++ 0x0A-python-inheritance/1-my_list.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+""" clase """
+
+
+class MyList(list):
+    """ funfion print sorted """
+    def print_sorted(self):
+        """imprimir lista ordenada"""
+        print(sorted(self))

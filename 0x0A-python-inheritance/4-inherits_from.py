972efb2ddde27517838cf99b7763cbe1723b73ba
NazarenaAranda <nazarenaaranda15@gmail.com>
2022-07-13 13:21:07 -0700

    condicione

--- 0x0A-python-inheritance/4-inherits_from.py
+++ 0x0A-python-inheritance/4-inherits_from.py
@@ -4,7 +4,6 @@
 
 def inherits_from(obj, a_class):
     """si el objeto es una instancia devuelve True"""
-    if isinstance(obj, a_class):
-        return True
-    else:
+    if a_class == type(obj):
         return False
+    return isinstance(obj, a_class)

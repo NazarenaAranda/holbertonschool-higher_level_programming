7b2d5ef82271e4d3447e169ce115ca42324fad6d
NazarenaAranda <nazarenaaranda15@gmail.com>
2022-07-13 19:01:55 -0700

    11

--- 0x0A-python-inheritance/11-square.py
+++ 0x0A-python-inheritance/11-square.py
@@ -16,7 +16,7 @@ class Square(Rectangle):
         def area(self):
             """metodo area"""
             return(super().area())
+
         def __str__(self):
             """ejecuta string"""
-            return("[Square] {}/{}".
-                    format(self.__size, self__size))
+            return("[Square] {}/{}".format(self.__size, self__size))

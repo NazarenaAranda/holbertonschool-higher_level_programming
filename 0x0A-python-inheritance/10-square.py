c62a5071c6de0f4775e108b112761d36d1aca43e
NazarenaAranda <nazarenaaranda15@gmail.com>
2022-07-13 18:53:40 -0700

    10 o fun

---
+++ 0x0A-python-inheritance/10-square.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+""" importar rectangle de "9-rectangle.py" """
+
+Rectangle = __import__(
+        "9-rectangle").Rectangle
+
+
+class Square(Rectangle):
+    """clase square"""
+    def __init__(self, size):
+        """instanciacion con size"""
+        self.__size = size
+        self.integer_validator("size", size)
+        super().__init__(size, size)
+
+        def area(self):
+            """metodo area"""
+            return(super().area())
+

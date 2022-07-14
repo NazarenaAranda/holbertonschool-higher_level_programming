f2fd576d1deec4af4cc6ff0a7e314e23d35c0cf0
NazarenaAranda <nazarenaaranda15@gmail.com>
2022-07-13 17:51:44 -0700

    7, exceptions

---
+++ 0x0A-python-inheritance/7-base_geometry.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+""" definiendo la base """
+
+
+class BaseGeometry():
+    """si el area no esta implementada lanza una excepcion"""
+    def area(self):
+        raise Exception("area() is not implemented")
+    def integer_validator(self, name, value):
+        """si no es entero tira una excepcion"""
+        if type(value) is not int:
+            raise TypeError(
+                    "{} must be an integer".format(name))
+        if value <= 0:
+            raise ValueError(
+                    "{} must be greater than 0".format(name
+                        ))

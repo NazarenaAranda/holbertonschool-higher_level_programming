11c92f5cddaa1466301820c854e3f6c4fc6e6ba2
NazarenaAranda <nazarenaaranda15@gmail.com>
2022-07-13 18:08:27 -0700

    8

---
+++ 0x0A-python-inheritance/8-rectangle.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+""" importar BaseGeometry de "7-base_geometry.py" """
+
+BaseGeometry = __import__(
+        "7-base_geometry").BaseGeometry
+
+
+class Rectangle(BaseGeometry):
+    """clase rectangle"""
+
+    def  __init__(self, width, height):
+        """instanciacion con width y height"""
+        self.__width = width
+        self.__height = height
+        self.integer_validator(" ", width)
+        self.integer_validator(" ", height)

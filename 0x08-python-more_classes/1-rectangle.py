325a45a70b5a66028e8856ebe11fae19625f07e2
NazarenaAranda <nazarenaaranda15@gmail.com>
2022-07-21 12:18:40 -0700

    1-rectangle

---
+++ 0x08-python-more_classes/1-rectangle.py
@@ -0,0 +1,37 @@
+#!/usr/bin/python3
+"""class Rectangle that defines a rectangle"""
+
+
+class Rectangle:
+    """defines a rectangle"""
+    def __init__(self, width=0, height=0):
+        self.width = width
+        self.height = height
+
+        @property
+        def width(self):
+            """ algo """
+            return self.__width
+
+        @width.setter
+        def width(self, value):
+            """ algp """
+
+            if not isinstance(self, int):
+                raise TypeError("width must be an integer")
+            if value < 0:
+                raise ValueError("width must be >= 0")
+
+        @property
+        def height(self):
+            """ algo """
+            return self.__height
+
+        @height.setter
+        def height(self, value):
+            """ algp """
+
+            if not isinstance(self, int):
+                raise TypeError("width must be an integer")
+            if value < 0:
+                raise ValueError("width must be >= 0")

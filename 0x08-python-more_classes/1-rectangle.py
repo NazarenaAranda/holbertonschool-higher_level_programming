e36e4c8dafd9f0aa1a60a731b8ce3af1cc8b548c
NazarenaAranda <nazarenaaranda15@gmail.com>
2022-07-21 13:15:34 -0700

    1

--- 0x08-python-more_classes/1-rectangle.py
+++ 0x08-python-more_classes/1-rectangle.py
@@ -32,6 +32,6 @@ class Rectangle:
             """ algp """
 
             if not isinstance(self, int):
-                raise TypeError("width must be an integer")
+                raise TypeError("height must be an integer")
             if value < 0:
-                raise ValueError("width must be >= 0")
+                raise ValueError("height must be >= 0")

91bd472260948b01c4ade8c4f16d87f14382fc56
NazarenaAranda <nazarenaaranda15@gmail.com>
2022-07-21 23:19:51 -0700

    2 y 3, antes de perderlo

--- 0x08-python-more_classes/2-rectangle.py
+++ 0x08-python-more_classes/2-rectangle.py
@@ -1,8 +1,10 @@
 #!/usr/bin/python3
-"""Write a class Rectangle"""
+""" Write a class Rectangle """
+
 
 class Rectangle:
-    """defines a rectangle """
+    """ Defines a rectangle """
+
     def __init__(self, width=0, height=0):
         self.height = height
         self.width = width
@@ -15,7 +17,7 @@ class Rectangle:
     def width(self, value):
         if type(value) != int:
             raise TypeError("width must be an integer")
-        if value < 0:
+        if (value < 0):
             raise ValueError("width must be >= 0")
         self.__width = value
 
@@ -30,3 +32,11 @@ class Rectangle:
         if value < 0:
             raise ValueError("height must be >= 0")
         self.__height = value
+
+    def area(self):
+        return self.__height * self.__width
+
+    def perimeter(self):
+        if self.__height == 0 or self.__width == 0:
+            return 0
+        return ((self.__height * 2) + (self.__width * 2))

def4b06d0f1a5c3bd8b67ceba32f4b9415e2d9d5
NazarenaAranda <nazarenaaranda15@gmail.com>
2022-07-25 13:28:54 -0700

    8

--- 0x0E-SQL_more_queries/8-cities_of_california_subquery.sql
+++ 0x0E-SQL_more_queries/8-cities_of_california_subquery.sql
@@ -3,6 +3,5 @@
 
 SELECT id, name
 FROM cities
-WHERE state_id = (SELECT id FROM states WHERE namae = "California")
-GROUP BY id ORDER BY id ASC;
-
+WHERE state_id = (SELECT id FROM
+states WHERE name = "California") ORDER BY id ASC;

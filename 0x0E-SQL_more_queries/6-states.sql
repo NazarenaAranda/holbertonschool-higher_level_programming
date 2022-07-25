efffd115164d3bc0ce893dad0a31ec507152a577
NazarenaAranda <nazarenaaranda15@gmail.com>
2022-07-25 13:10:05 -0700

    6,7

--- 0x0E-SQL_more_queries/6-states.sql
+++ 0x0E-SQL_more_queries/6-states.sql
@@ -2,7 +2,9 @@
 -- table states (in the database hbtn_0d_usa) on your MySQL
 -- server
 
-CREATE DATABASE IF NOT EXISTS hbtn_0d_usa (
+CREATE DATABASE IF NOT EXISTS hbtn_0d_usa;
+USE hbtn_0d_usa;
+CREATE TABLE IF NOT EXISTS states (
 	id INT unique AUTO_INCREMENT NOT NULL PRIMARY KEY,
 	name VARCHAR(256) NOT null
 );
--- 0x0E-SQL_more_queries/7-cities.sql
+++ 0x0E-SQL_more_queries/7-cities.sql
@@ -7,6 +7,6 @@ USE hbtn_0d_usa;
 CREATE TABLE IF NOT EXISTS cities (
 	id INT unique AUTO_INCREMENT NOT NULL PRIMARY KEY,
 	name VARCHAR(256) NOT null,
-	state_id INT NO NULL,
+	state_id INT NOT NULL,
 	FOREIGN KEY(state_id) REFERENCES hbtn_0d_usa.states(id)
 );

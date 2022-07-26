bd2b82031456f1d79587304eb0ff29cceb01b14b
NazarenaAranda <nazarenaaranda15@gmail.com>
2022-07-26 13:26:57 -0700

    100

--- 0x09-python-everything_is_object/100-magic_string.py
+++ 0x09-python-everything_is_object/100-magic_string.py
@@ -1,4 +1,4 @@
 #!/usr/bin/python3
-def magic_string(l=[]):
-    l.append("BestSchool")
-    return ', '.join([str(item) for item in l])
+def magic_string(li=[]):
+    li.append("BestSchool")
+    return ', '.join([str(item) for item in li])

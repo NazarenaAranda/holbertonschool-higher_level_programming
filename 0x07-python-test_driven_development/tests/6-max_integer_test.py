c630b42dc0fa0c08a6ac1b1deb90efa62b634dfa
NazarenaAranda <nazarenaaranda15@gmail.com>
2022-07-27 19:24:43 -0700

    6

--- 0x07-python-test_driven_development/tests/6-max_integer_test.py
+++ 0x07-python-test_driven_development/tests/6-max_integer_test.py
@@ -8,16 +8,16 @@ class TestMaxInteger(unittest.TestCase):
     """class TestMaxInteger"""
     def test0(self):
         with self.assertRaises(TypeError):
-            max_integer(["naza", 1, 3, 5])
+            max_integer(['naza', 6, 3, 7])
 
     def test1(self):
         self.assertEqual(max_integer([]), None)
 
     def test2(self):
-        self.assertEqual(max_integer([9, 9, 9, 9, 9, 9]), 9)
+        self.assertEqual(max_integer([7, 7, 7, 7, 7, 7]), 7)
 
     def test3(self):
-        self.assertEqual(max_integer([0]), 1)
+        self.assertEqual(max_integer([-1, 6, 7, 3, -5, 51]), 51)
 
 if __name__ == '__main__':
     unittest.main()

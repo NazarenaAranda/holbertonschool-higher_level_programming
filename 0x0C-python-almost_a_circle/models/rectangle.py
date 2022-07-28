561f68b3e376455f8433df28d0aa7adff26b5867
NazarenaAranda <nazarenaaranda15@gmail.com>
2022-07-28 08:10:42 -0700

    no

--- 0x0C-python-almost_a_circle/models/rectangle.py
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -70,3 +70,43 @@ class Rectangle(Base):
             print(f" " * self.__x, end="")
             print(f"#" * self.__width)
 
+    def _str_(self):
+    st = "[Rectangle] ({}) {}/
+{} - {}/{}".format(
+            self.id, self._x, self.y
+, self.width, self._height)
+        return st
+
+    def update(self, *args, **kwargs):
+        if args is not None and len(args) != 0:
+            if len(args) >= 1:
+                if type(args[0]) != int and args[0] is not None:
+                    raise TypeError("id must be an integer")
+                self.id = args[0]
+            if len(args) > 1:
+                self.width = args[1]
+            if len(args) > 2:
+                self.height = args[2]
+            if len(args) > 3:
+                self.x = args[3]
+            if len(args) > 4:
+                self.y = args[4]
+        else:
+            for key, value in kwargs.items():
+                if key == "id":
+                    if type(value) != int and value is not None:
+                        raise TypeError("id must be an integer")
+                    self.id = value
+                if key == "width":
+                    self.width = value
+                if key == "height":
+                    self.height = value
+                if key == "x":
+                    self.x = value
+                if key == "y":
+                    self.y = value
+
+    def to_dictionary(self):
+        my_dict = {'id': self.id,'width': self.__width, 'height': self._height,
+                'x': self.x, 'y': self._y}:
+        return my_dict

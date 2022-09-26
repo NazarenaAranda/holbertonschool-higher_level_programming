9975be5418223e612e578196922e57c01b781a47
Nazarena <nazarenaaranda15@gmail.com>
2022-09-25 19:08:27 -0700

    load

---
+++ python-import_modules/2-args.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+from sys import argv
+
+if __name__ == "__main__":
+    args = len(argv) - 1
+    if args == 0:
+        print("0 arguments.")
+
+    elif args == 1:
+        print("1 argument:")
+        print("1: {}".format(argv[1]))
+    else:
+        print(f"{args} arguments:".format(args))
+        for i in range(1, args + 1):
+            print(f"{i}: {argv[i]}")
---
+++ python-import_modules/3-infinite_add.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+from sys import argv
+
+if __name__ == "__main__":
+    args = argv
+    i = 0
+    for j in range(1, len(args)):
+        i = i + int(args[j])
+    print("{}".format(i))

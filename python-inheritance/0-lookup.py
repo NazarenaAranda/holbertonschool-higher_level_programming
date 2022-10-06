c92023aa26b900ddfefb057e4f8a8818bbd04d9c
Nazarena <nazarenaaranda15@gmail.com>
2022-10-06 05:07:46 -0700

    documented

--- python-inheritance/0-lookup.py
+++ python-inheritance/0-lookup.py
@@ -1,4 +1,7 @@
 #!/usr/bin/python3
+"""the list of available attributes and methods of an object"""
+
+
 def lookup(obj):
     """retorna lista de atributos y métodos de un objeto"""
     return list(dir(obj))

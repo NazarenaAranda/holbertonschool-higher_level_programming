79c284710a1c05d12336600b869b14f0376e740f
Nazarena <nazarenaaranda15@gmail.com>
2022-11-07 14:43:44 -0800

    pycodestyle

--- python-test_driven_development/3-say_my_name.py
+++ python-test_driven_development/3-say_my_name.py
@@ -9,4 +9,3 @@ def say_my_name(first_name, last_name=""):
     elif type(last_name) is not str:
         raise TypeError("last_name must be a string")
     print(f"My name is {first_name} {last_name}")
-

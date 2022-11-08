62695b3e17698f951a93f1b1f43c8df1b79a400a
Nazarena <nazarenaaranda15@gmail.com>
2022-11-07 20:11:16 -0800

    four and tests

---
+++ python-test_driven_development/4-print_square.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+"""function that prints a square with the character #"""
+
+
+def print_square(size):
+    """print square"""
+    for _ in range(size):
+        print("#" * size)
+    if type(size) is not int:
+        raise TypeError("size must be an integer")
+    elif size < 0:
+        raise ValueError("size must be >= 0")
+    elif type(size) is float and size < 0:
+        raise TypeError("size must be an intege")

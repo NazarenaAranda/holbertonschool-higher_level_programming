8c6f38f523dfb1e3b45fc7caaac838867315294e
Nazarena <nazarenaaranda15@gmail.com>
2022-11-18 20:35:41 -0800

    &

--- python-input_output/7-add_item.py
+++ python-input_output/7-add_item.py
@@ -6,6 +6,7 @@ load_from_json_file = __import__('6-load_from_json_file').load_from_json_file
 
 
 filename = "add_item.json"
+
 try:
     list_json = load_from_json_file(filename)
 except:
@@ -15,4 +16,3 @@ for arg in argv[1:]:
     list_json.append(arg)
 
 save_to_json_file(list_json, filename)
-

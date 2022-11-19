0332fdacb27a9d72b4594e12c3dd2150f424859a
Nazarena <nazarenaaranda15@gmail.com>
2022-11-18 20:33:20 -0800

    7

---
+++ python-input_output/7-add_item.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+""" script that adds all arguments to a Python list"""
+from sys import argv
+save_to_json_file = __import__('5-save_to_json_file').save_to_json_file
+load_from_json_file = __import__('6-load_from_json_file').load_from_json_file
+
+
+filename = "add_item.json"
+try:
+    list_json = load_from_json_file(filename)
+except:
+    list_json = []
+
+for arg in argv[1:]:
+    list_json.append(arg)
+
+save_to_json_file(list_json, filename)
+

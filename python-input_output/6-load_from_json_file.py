935dddaa4e03c8f83f8fc2294fa96fb4c3045cd7
Nazarena <nazarenaaranda15@gmail.com>
2022-11-18 20:21:02 -0800

    6

---
+++ python-input_output/6-load_from_json_file.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+"""function that creates an Object from a “JSON file"""
+import json
+
+
+def load_from_json_file(filename):
+    """function"""
+    with open(filename, 'r', encoding='utf=8') as f:
+        return json.load(f)
